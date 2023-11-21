751e5b759687d593fa455c28328852a2241fe3bc
dmitrykoptev <dmitrykoptev23@gmail.com>
2023-11-21 14:01:21 +0300

    fix: deleted unused variables

--- src/components/notes/NotesList.tsx
+++ src/components/notes/NotesList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import classes from "./NotesList.module.scss";
 import Note from "./Note";
 import { INote } from "../../models/Note";
